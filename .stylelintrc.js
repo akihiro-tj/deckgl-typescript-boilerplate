fc8b1c5266d11f57aa1fe12d97f476a5121488eb
Akihiro Tojo <akihiro.021109@gmail.com>
2021-12-08 23:08:26 +0900

    Lint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,8 +1,13 @@
 module.exports = {
   extends: [
     'stylelint-config-standard',
+    'stylelint-config-standard-scss',
     'stylelint-config-recommended',
     'stylelint-config-recess-order',
   ],
   plugins: ['stylelint-scss'],
+  rules: {
+    'block-no-empty': null,
+    'selector-class-pattern': null,
+  },
 };
