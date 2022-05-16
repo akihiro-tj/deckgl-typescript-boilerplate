3ac52e0d5af04e16df97584ba608c99813095dd5
Akihiro Tojo <akihiro.02.tj@gmail.com>
2022-05-16 20:36:12 +0900

    Add map

---
+++ src/components/Attribution.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface IProps {
+  contributors: TContributor[];
+}
+
+type TContributor = {
+  name: string;
+  url: string;
+};
+
+export const Attribution: React.FC<IProps> = ({ contributors }) => {
+  const Wrapper = styled.div`
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    padding: 0 5px;
+
+    background: rgba(255, 255, 255, 0.8);
+    color: #333;
+    line-height: 1.4;
+    font-size: 0.75rem;
+  `;
+
+  const Link = styled.a``;
+
+  return (
+    <Wrapper>
+      &copy;&nbsp;
+      {contributors.map(contributor => (
+        <>
+          <Link
+            key={contributor.name}
+            href={contributor.url}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {contributor.name}
+          </Link>
+          &nbsp;
+        </>
+      ))}
+      contributors
+    </Wrapper>
+  );
+};
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,36 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
-import { Radio } from '../components/Radio';
+import { Map } from '../components/Map';
 
 export const App: React.FC = () => {
-  const values = ['A', 'B', 'C'];
-  const [value, setValue] = useState(values[0]);
-
-  const handleRadioClick: React.MouseEventHandler<HTMLButtonElement> = e => {
-    const clicked = e.currentTarget.getAttribute('data-value');
-    if (!clicked) return;
-    setValue(clicked);
-  };
-
-  const Wrapper = styled.div`
-    padding: 1.5rem;
-  `;
-
-  const Paragraph = styled.p`
-    font-size: 18px;
-  `;
-
-  const Value = styled.span`
-    font-weight: 700;
-  `;
+  const Wrapper = styled.div``;
 
   return (
     <Wrapper>
-      <Radio values={values} curValue={value} onClick={handleRadioClick} />
-      <Paragraph>
-        <Value>{value}</Value> is selected
-      </Paragraph>
+      <Map />
     </Wrapper>
   );
 };
